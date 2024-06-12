11cc85184d52b7d76a7b37d9fdde36301cec6369
kmcs2727 <kimura.ryosuke.git@gmail.com>
2024-06-12 11:50:25 +0900

    デプロイエラー改善

--- app/user/mypage/page.tsx
+++ app/user/mypage/page.tsx
@@ -7,7 +7,6 @@ import { Button } from "@/components/ui/button";
 export default function MyPage() {
   const loginUserEmail = useAuth();
   const [itemsByPerson, setItemsByPerson] = useState([]);
-  const [loading, setLoding] = useState(true);
 
   useEffect(() => {
     const getItemsByPerson = async() => {
@@ -23,36 +22,28 @@ export default function MyPage() {
       });
       const jsonData = await response.json();
       setItemsByPerson(jsonData.itemsByPerson);
-      setLoding(false);
     }
     getItemsByPerson();
-  }, [loading]);
+  }, [loginUserEmail]);
 
-  if(loading) {
+  return (
     <div className="p-6 items-center justify-center">
-      <h1 className="font-bold text-2xl mb-6">読み込み中...</h1>
-    </div>
-  }
-
-  if(itemsByPerson.length > 0){
-    return (
-      <div className="p-6 items-center justify-center">
-        <h1 className="font-bold text-2xl mb-6">プロフィール設定</h1>
-        <Button variant={'outline'}>更新</Button>
-        <h1 className="font-bold text-2xl mb-6">あなたの記事</h1>
-        <h2>{itemsByPerson.length}件</h2>
-        <div className="grid grid-cols-2 gap-20">
-          {itemsByPerson?.map((item: any) => (
-            <div key={item._id} className="border p-2 rounded-lg relative">
-              <Link href={`/item/readsingle/${item._id}`}>
-                <h3 className="font-bold">{item.title}</h3>
-                <p>{item.content.substring(0, 20)}</p>
-                <span className="absolute inset-0"></span>
-              </Link>
-            </div>
-          ))}
-        </div>
+      <h1 className="font-bold text-2xl mb-6">プロフィール設定</h1>
+      <Button variant={'outline'}>更新</Button>
+      <h1 className="font-bold text-2xl mb-6">あなたの記事</h1>
+      <h2>{itemsByPerson.length}件</h2>
+      <div className="grid grid-cols-2 gap-20">
+        {itemsByPerson?.map((item: any) => (
+          <div key={item._id} className="border p-2 rounded-lg relative">
+            <Link href={`/item/readsingle/${item._id}`}>
+              <h3 className="font-bold">{item.title}</h3>
+              <p>{item.content.substring(0, 20)}</p>
+              <span className="absolute inset-0"></span>
+            </Link>
+          </div>
+        ))}
       </div>
-    );
-  }
+    </div>
+  );
+
 }
