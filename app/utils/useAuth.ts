3e98ca558e82a5d98db89936494e7e0b0e9ea082
kmcs2727 <kimura.ryosuke.git@gmail.com>
2024-06-16 14:10:05 +0900

    トースト追加

--- app/utils/useAuth.ts
+++ app/utils/useAuth.ts
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { jwtVerify } from "jose";
+import { useToast } from "@/components/ui/use-toast";
 
 interface JwtPayload {
 	email: string
@@ -9,11 +10,16 @@ interface JwtPayload {
 
 const useAuth = () => {
 	const [loginUserEmail, setLoginUserEmail] = useState<string>("");
+	const toast = useToast();
 	const router = useRouter();
 	useEffect(() => {
 		const checkToken = async() => {
 			const token = localStorage.getItem("token");
 			if(!token){
+				toast.toast({
+					title: "ログインしてください",
+					variant: "destructive",
+				});
 				router.push("/user/login");
 				return;
 			}
@@ -22,6 +28,10 @@ const useAuth = () => {
 				const decodedJwt = await jwtVerify(token, secretKey) as { payload: JwtPayload };
 				setLoginUserEmail(decodedJwt.payload.email);
 			} catch(err) {
+				toast.toast({
+					title: "ログインしてください",
+					variant: "destructive",
+				});
 				router.push("/user/login");
 				return;
 			}
